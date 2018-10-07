599f17bf6f87f1be39e43803f4c545d13fd46b03
alexp <alexplotbox@teamlead.ru>
2018-10-07 14:30:04 +0300

    add api function to post new invoice

--- src/api/index.js
+++ src/api/index.js
@@ -4,3 +4,14 @@ export async function fetchInvoices () {
   let response = await fetch(`${API_URL}/invoices`)
   return response.json()
 }
+
+export async function postInvoice (invoice) {
+  await fetch(`${API_URL}/invoices`, {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(invoice)
+  })
+}

5d96e7451d268a787ee8718199686ca1a1883c53
alexp <alexplotbox@teamlead.ru>
2018-10-07 13:41:11 +0300

    add form for invoice creation

--- src/components/InvoiceCreatePage.vue
+++ src/components/InvoiceCreatePage.vue
@@ -1,14 +1,71 @@
 <template>
   <div class="container">
     <base-page-title title="Create invoice"></base-page-title>
+    <base-container>
+      <a-form @submit="handleSubmit" :autoFormCreate="(form)=>{this.form = form}">
+        <a-row>
+          <a-col>
+            <a-form-item label="Number:" fieldDecoratorId="number">
+              <a-input placeholder="Enter invoice number"/>
+            </a-form-item>
+          </a-col>
+        </a-row>
+        <a-row type="flex" justify="start" :gutter="10">
+          <a-col>
+            <a-form-item label="Invoice Date:" fieldDecoratorId="invoiceDate">
+              <a-date-picker/>
+            </a-form-item>
+          </a-col>
+          <a-col>
+            <a-form-item label="Supply Date" fieldDecoratorId="supplyDate">
+              <a-date-picker/>
+            </a-form-item>
+          </a-col>
+        </a-row>
+        <a-row>
+          <a-col :span="24">
+            <a-form-item label="Comment" fieldDecoratorId="comment">
+              <a-textarea :autosize="{ minRows: 2, maxRows: 6 }" />
+            </a-form-item>
+          </a-col>
+        </a-row>
+        <a-row type="flex" justify="end">
+          <a-col>
+            <a-form-item>
+              <a-button type='primary' htmlType='submit'>
+                Save
+              </a-button>
+            </a-form-item>
+          </a-col>
+        </a-row>
+      </a-form>
+    </base-container>
   </div>
 </template>
 
 <script>
 import BasePageTitle from './BasePageTitle'
+import BaseContainer from './BaseContainer'
+import AFormItem from 'ant-design-vue/es/form/FormItem'
+import ARow from 'ant-design-vue/es/grid/Row'
+import ACol from 'ant-design-vue/es/grid/Col'
 export default {
   name: 'InvoiceCreatePage',
-  components: {BasePageTitle}
+  data () {
+    return {
+    }
+  },
+  methods: {
+    handleSubmit (e) {
+      e.preventDefault()
+      this.form.validateFields((err, values) => {
+        if (!err) {
+          console.log('Received values of form: ', values)
+        }
+      })
+    }
+  },
+  components: {ACol, ARow, AFormItem, BaseContainer, BasePageTitle}
 }
 </script>
 
